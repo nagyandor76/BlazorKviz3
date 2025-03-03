fb6bc66d5e113a3255bf18bbda761df0942f6b8c
nagyandor76 <nagyandor76@gmail.com>
2025-03-03 01:43:04 +0100

    Refactor and sound

--- wwwroot/quiz.js
+++ wwwroot/quiz.js
@@ -6,6 +6,27 @@
         const utterance = new SpeechSynthesisUtterance(text);
         utterance.lang = lang;
         window.speechSynthesis.speak(utterance);
+    },
+    playHappySound: function () {
+        let audio = new Audio('/Sounds/happy.mp3');
+        if (audio) {
+            audio.play();
+        }
+    },
+    playSadSound: function () {
+        let audio = new Audio('/Sounds/sad.mp3');
+        if (audio) {
+            audio.play();
+        }
+    },
+    saveFile: function (filename, content) {
+        const element = document.createElement('a');
+        const file = new Blob([content], { type: 'application/json' });
+        element.href = URL.createObjectURL(file);
+        element.download = filename;
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     }
 };
 
